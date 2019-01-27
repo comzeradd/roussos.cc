88006234a0257386cef2367fcbcb228981cccb95
Nikos Roussos <nikos@roussos.cc>
2019-01-27 19:21:43 +0200

    Simplify pgp modal

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
     'use strict';
 
+    // handle copy events
+    $('#copy').click(function() {
+        const target = $(this).data('target');
+        const copyText = document.getElementById(target);
+        copyText.select();
+        document.execCommand('copy');
+        $('#copied-' + target).addClass('highlight');
+    });
+
+    // tooltips
+    $('[data-toggle="tooltip"]').tooltip();
 });
